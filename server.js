df69594bba320dbb55be303f3ce5c20eff5cffe7
nogueirapedrolucas1-star <nogueirapedrolucas1@gmail.com>
2026-01-22 14:10:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -14,27 +14,15 @@ async function start() {
   });
 
   sock.ev.on("creds.update", saveCreds);
-
-  sock.ev.on("connection.update", async (update) => {
-    if (update.qr) {
-      qrBase64 = await QRCode.toDataURL(update.qr);
-      console.log("QR CODE GERADO");
-    }
-  });
 }
 
 start();
 
-app.get("/", (req, res) => {
-  res.send("Servidor WhatsApp rodando");
-});
+// ================================
+// CONFIGURAÇÃO DA PORTA DO SERVIDOR
+// ================================
+const PORT = process.env.PORT || 3000;
 
-app.get("/qr", (req, res) => {
-  if (!qrBase64) return res.send("QR ainda não gerado, aguarde 10 segundos e recarregue.");
-  res.json({ qr: qrBase64 });
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
 });
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Servidor iniciado");
-});
-
